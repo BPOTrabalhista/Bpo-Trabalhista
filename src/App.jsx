439c25a5738c131a6e23f04fb5999ba71dc7597a
BPOTrabalhista <deoliveira.aemp@gmail.com>
2026-05-13 14:57:55 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useEffect } from "react";
+import { useState, useMemo, useEffect, useRef } from "react";
 import * as XLSX from 'xlsx';
 import {
   getTarefas, upsertTarefa, deleteTarefa, deleteTarefasEmLote,
